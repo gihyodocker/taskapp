4194c6fd7856bdeddc938b3662856f86135a5b6d
Akinori Yamada <stormcat24@stormcat.io>
2023-07-02 02:25:46 +0900

    Add migrator container

---
+++ containers/migrator/migrate.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+if [ "$#" -ne 6 ]; then
+  echo "usage: $0 <db_host> <db_port> <db_name> <username> <password> <command>"
+  exit 1
+fi
+
+db_host=$1
+db_port=$2
+db_name=$3
+db_username=$4
+db_password=$5
+command=$6
+
+echo "Waiting for MySQL to start..."
+until mysql -h $db_host -P $db_port -u $db_username -p$db_password -e "show databases;" &> /dev/null; do
+  >&2 echo "MySQL is unavailable - sleeping"
+  sleep 1
+done
+echo "MySQL is up - executing command"
+
+migrate -source file://./history \
+  -database mysql://$db_username:$db_password@tcp\($db_host:$db_port\)/$db_name $command
