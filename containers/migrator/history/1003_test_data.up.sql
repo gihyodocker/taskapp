6e7577cb9d205f965f1ff5ea0478689b7405b826
Akinori Yamada <stormcat24@stormcat.io>
2023-07-17 19:30:32 +0900

    Add test data

---
+++ containers/migrator/history/1003_test_data.up.sql
@@ -0,0 +1,8 @@
+INSERT INTO task (id, title, content, status, created, updated)
+VALUES ('01H4QEZ39FBP67SS9V042ZJ5H1', 'Dockerのインストール', 'Docker Desktopでのローカル開発環境準備', 'DONE', NOW(), NOW()),
+       ('01H4QEZ39FZVW6Y6HVQDHQ192K', 'asdfのインストール', 'asdfでのツールの管理', 'DONE', NOW(), NOW()),
+       ('01H4QEZ39F0MCJERZ7BFHSG92E', 'Kubernetesの検証', 'Kubernetesをどのように導入するか', 'PROGRESS', NOW(), NOW()),
+       ('01H4QEZ39FHB1QQJAYJEN373VA', 'パブリッククラウドの選定', 'AWS/GCP/Azureどれかを検証する', 'PROGRESS', NOW(), NOW()),
+       ('01H4QEZ39F1ZKC2T54SAYQZPCC', 'コンテナイメージのCI', 'GitHub Actionsでコンテナイメージをビルド', 'PROGRESS', NOW(), NOW()),
+       ('01H4QEZ39F1NMC3CKQ8BPSCNA8', '継続的デリバリーの検証', 'CDツールをいくつか検証する', 'BACKLOG', NOW(), NOW()),
+       ('01H4QEZ39FKNA78DZQ6CGSCJJM', 'OrbStackの検証', 'OrbStackでの開発環境を検証', 'BACKLOG', NOW(), NOW());
